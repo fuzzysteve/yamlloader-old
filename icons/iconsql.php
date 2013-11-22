368a1daf644cd2acfcfcc5fe1a5672d2a4f71ce0
Steve Anderson <steve@fuzzwork.co.uk>
2013-11-22 12:18:53 +0100

    Adding icon loader php

---
+++ icons/iconsql.php
@@ -0,0 +1,35 @@
+<?
+/*
+The table structure already exists as eveIcons:
+*/
+
+
+$dbh = new PDO('mysql:host=localhost;dbname=eve', 'eve', 'eve');
+
+
+$typeids=yaml_parse_file("iconIDs.yaml");
+
+$iconsql="insert into eveIcons (iconid,description,iconFile) values (:typeid,:description,:iconfile) on duplicate key update iconFile=:iconfile";
+$iconnodescsql="insert into eveIcons (iconid,iconFile) values (:typeid,:iconfile) on duplicate key update iconFile=:iconfile";
+
+$iconstmt=$dbh->prepare($iconsql);
+$iconnodescstmt=$dbh->prepare($iconnodescsql);
+
+
+
+
+
+foreach ($typeids as $typeid => $details)
+{
+    echo "$typeid\n";
+    if (isset($details["description"]))
+    {
+    $iconstmt->execute(array(":typeid"=>$typeid,":description"=>$details["description"],":iconfile"=>$details["iconFile"]));
+    }
+    else
+    {
+    $iconnodescstmt->execute(array(":typeid"=>$typeid,":iconfile"=>$details["iconFile"]));
+    }
+   
+}
+
